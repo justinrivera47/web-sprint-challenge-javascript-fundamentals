e9eebc5dfc7d2fbbca662fdc28f9d8fc5b1d0663
justinrivera47 <justinrivera47@gmail.com>
2021-10-21 12:45:44 -0400

    finished the sprint.

--- index.js
+++ index.js
@@ -30,13 +30,14 @@ myFunction();
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
 function summation(num) {
-
-  for(let i = 1; i < num; i++) {
-console.log();
+  let sum = 0;
+  for(let i = 0; i <= num; i++) {
+    sum += i
   }
-  console.log(num)
+  return sum
 }
-console.log(summation(7));
+
+
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
